77f0603098934db6b3c9ee9513bd3f24dfc3bc7f
T. Thiery <thomas.thiery+git@gmail.com>
2021-06-12 11:38:08 +0200

    Fix UseRoutingKey type constraint.

--- src/VioletGrass.Middleware/Router/StringRouter/IMiddlewareBuilderExtensions_StringRouter.cs
+++ src/VioletGrass.Middleware/Router/StringRouter/IMiddlewareBuilderExtensions_StringRouter.cs
@@ -5,7 +5,7 @@ namespace VioletGrass.Middleware
 {
     public static class IMiddlewareBuilderExtensions_StringRouter
     {
-        public static IMiddlewareBuilder<TContext> UseRoutingKey<TContext>(this IMiddlewareBuilder<TContext> self, Func<Context, string> routingKeySelector, params string[] routePatterns) where TContext : Context
+        public static IMiddlewareBuilder<TContext> UseRoutingKey<TContext>(this IMiddlewareBuilder<TContext> self, Func<TContext, string> routingKeySelector, params string[] routePatterns) where TContext : Context
         {
             return self.Use(StringRouter.CreateRoutingKeyMiddlewareFactory<TContext>(routingKeySelector, routePatterns));
         }
